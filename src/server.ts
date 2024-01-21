4243caf3315733ffc35ebe7083332039fc27274b
Daniel González Briseño <qbixmex@gmail.com>
2024-01-21 15:11:26 -0800

    rest-server

--- src/server.ts
+++ src/server.ts
@@ -22,8 +22,33 @@ class Server {
     });
 
     //* Middlewares
+    this.app.use('/api/todos', (_request: Request, response: Response) => {
+      return response.json([
+        {
+          id: 'b981e38f',
+          title: 'Go to the GYM',
+          done: true,
+          createdAt: new Date('2024-01-12T14:02:10.135Z'),
+          updatedAt: new Date('2024-01-12T14:08:35.455Z'),
+        },
+        {
+          id: '8b2fc14e',
+          title: 'Go to the Job',
+          done: true,
+          createdAt: new Date('2024-01-12T15:15:06.717Z'),
+          updatedAt: new Date('2024-01-12T15:18:35.132Z'),
+        },
+        {
+          id: 'ce0b4af2',
+          title: 'Finish Homework',
+          done: false,
+          createdAt: new Date('2024-01-12T15:25:25.243Z'),
+          updatedAt: new Date('2024-01-12T15:28:35.432Z'),
+        },
+      ]);
+    });
 
-    //* Public Folder
+    //* Single Page Application (SPA) like React, Vue, Angular, Solid, Qwik, etc.
     this.app.use(express.static('public'));
 
     this.app.get('*', (_request: Request, response: Response) => {
