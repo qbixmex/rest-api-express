44aa0f6d790c41e8e19ce35c1d0abfd701a6fe88
Daniel González Briseño <qbixmex@gmail.com>
2024-01-21 14:09:10 -0800

    default-port-fix

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -2,6 +2,6 @@ import 'dotenv/config';
 import { get } from 'env-var';
 
 export default {
-  PORT: get('PORT').required().asPortNumber(),
+  PORT: get('PORT').default(3000).asPortNumber(),
   PUBLIC_PATH: get('PUBLIC_PATH').default('public').asString(),
 };
