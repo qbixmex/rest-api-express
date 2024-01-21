b8cb97dd9bdb8d406b02d27f0d372bb282d4def2
Daniel González Briseño <qbixmex@gmail.com>
2024-01-21 13:16:19 -0800

    serving-react

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import path from 'path';
 
 class Server {
   private app = express();
@@ -14,6 +15,12 @@ class Server {
 
     //* Public Folder
     this.app.use(express.static('public'));
+
+    this.app.get('*', (_request: Request, response: Response) => {
+      const indexPath = path.join(__dirname, '../public/index.html');
+      console.log(indexPath)
+      response.sendFile(indexPath);
+    });
   }
 }
 
