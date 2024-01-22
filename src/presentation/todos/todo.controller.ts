1906a04f9ec02d8249bbca322cee624365b2302c
Daniel González Briseño <qbixmex@gmail.com>
2024-01-21 21:19:19 -0800

    update-todo

--- src/presentation/todos/todo.controller.ts
+++ src/presentation/todos/todo.controller.ts
@@ -64,18 +64,40 @@ class TodoController {
     //* Add newTodo to todos array
     todos.push(newTodo);
 
-    return response.json({
+    return response.status(201).json({
      ok: true,
      todo: newTodo,
     });
   }
 
   public updateTodo = (
-    request: Request,
+    request: Request<{ id: string }, {}, {
+      title?: string;
+      done?: boolean;
+    }>,
     response: Response
   ) => {
+    const id = request.params.id;
+
+    const todo = todos.find(todo => todo.id === id);
+
+    if (!todo) {
+      return response.status(404).json({
+        ok: false,
+        error: `Todo with id: ${id}, not found`,
+      });
+    }
+
+    const { title, done } = request.body;
+
+    //* Update todo in todos array
+    todo.title = title ?? todo.title;
+    todo.done = done ?? todo.done;
+    todo.updatedAt = new Date() ?? todo.updatedAt;
+
     return response.json({
-      message: "Update Todo not implemented."
+      ok: true,
+      todo,
     });
   }
 
