744fc6fa65cceb6262969ecbfba4220b59150cc2
Daniel González Briseño <qbixmex@gmail.com>
2024-02-05 10:24:10 -0800

    tests-on-get-todos

--- tests/presentation/todos/routes.test.ts
+++ tests/presentation/todos/routes.test.ts
@@ -1,7 +1,13 @@
 import request from 'supertest';
 import { testServer } from '../../test-server';
+import prisma from '../../../src/presentation/todos/data/postgres';
 
 describe('Test on Routes', () => {
+
+  const todos = [
+    { title: 'Test todo 1' },
+    { title: 'Test todo 2' },
+  ];
 
   beforeAll(async () => {
     await testServer.start();
@@ -12,11 +18,27 @@ describe('Test on Routes', () => {
   });
 
   test('Should return todos api/todos', async () => {
+
+    await prisma.todo.deleteMany();
+    await prisma.todo.createMany({
+      data: todos
+    });
     
-    await request(testServer.app)
+    const { body } = await request(testServer.app)
       .get('/api/v1/todos')
       .expect(200);
 
+    expect(body).toBeInstanceOf(Array);
+    expect(body.length).toBe(2);
+
+    body.forEach((todo: any, index: number) => {
+      expect(todo).toEqual({
+        id: expect.any(String),
+        title: todos[index].title,
+        completedAt: null,
+      });
+    });
+    
   });
 
 });
